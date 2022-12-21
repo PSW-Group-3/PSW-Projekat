3d4f944450d8043257b320a3893226447092b132
Luka Mandic <mandic.ra101.2019@uns.ac.rs>
2022-12-21 14:08:18 +0100

    Basic ui added

---
+++ PSW-projekat/PZP-Angular/src/app/modules/hospital/services/advertisement.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { AdvertisementService } from './advertisement.service';
+
+describe('AdvertisementService', () => {
+  let service: AdvertisementService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AdvertisementService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
