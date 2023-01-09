4bc80c4d8cd425a07d9d838d3d9e35452db0561b
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2023-01-09 11:40:27 +0100

    Add column for blood type in component

--- PSW-projekat/PZL-Angular/src/app/modules/hospital/blood-requests/blood-requests.component.ts
+++ PSW-projekat/PZL-Angular/src/app/modules/hospital/blood-requests/blood-requests.component.ts
@@ -15,7 +15,7 @@ export class BloodRequestsComponent implements OnInit {
   public requests: BloodRequest[]=[]
   public bloodType: String;
   public dataSource = new MatTableDataSource<BloodRequest>();
-  public displayedColumns = ['bloodQuantity', 'reason','date'];
+  public displayedColumns = ['bloodType','bloodQuantity', 'reason','date'];
   
     
   constructor(private bloodRequestService: BloodResuestService, private router: Router) { }
@@ -26,9 +26,11 @@ export class BloodRequestsComponent implements OnInit {
     })  }
 
   public GetRequests() {
+    console.log(this.ConvertToNumber(this.bloodType));
     this.bloodRequestService.getBloodRequestsByType(this.ConvertToNumber(this.bloodType)).subscribe(res => {
       this.requests = res;
       this.dataSource.data = this.requests;
+      console.log( this.dataSource.data)
     })
   }
 
@@ -37,15 +39,30 @@ export class BloodRequestsComponent implements OnInit {
 
   public ConvertToNumber(obj: any): any{
     switch(obj){
-      case 'ON': return 0;
-      case 'AN': return 1;
-      case 'BN': return 2;
-      case 'ABN': return 3;
-      case 'OP': return 4;
-      case 'AP': return 5;
-      case 'BP': return 6;
-      case 'ABP': return 7;
+      case 'O-': return 7;
+      case 'A-': return 4;
+      case 'B-': return 5;
+      case 'AB-': return 6;
+      case 'O+': return 3;
+      case 'A+': return 0;
+      case 'B+': return 1;
+      case 'AB+': return 2;
       default: return 0; 
+    }
+  }
+
+  
+  public ConvertToString(obj: any): any{
+    switch(obj){
+      case 0: return 'O-';
+      case 1: return 'A-';
+      case 2: return 'B-';
+      case 3: return 'AB-';
+      case 4: return 'O+';
+      case 5: return 'A+';
+      case 6: return 'B+';
+      case 7: return 'AB+';
+      default: return 'A+'; 
     }
   }
 
