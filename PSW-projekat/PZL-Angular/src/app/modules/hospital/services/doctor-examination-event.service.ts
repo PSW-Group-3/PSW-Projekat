3213e16a9388dfc351efd91b45de5e35fe07754b
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2023-01-11 12:25:26 +0100

    Add class diagram for doctor examination aggregate

---
+++ PSW-projekat/PZL-Angular/src/app/modules/hospital/services/doctor-examination-event.service.ts
@@ -0,0 +1,46 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DoctorExaminationDTO } from '../model/doctorExaminationDTO';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorExaminationEventService {
+
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+
+  DoctorExaminationAggregateStartTime(): Observable<number> {
+    return this.http.get<number>('api/DoctorExaminationEvent/DoctorExaminationAggregateStartTime/' + localStorage.getItem("currentUserId"), {headers: this.headers});
+  }
+
+  DoctorExaminationAggregateEndTime(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/DoctorExaminationAggregateEndTime', dto, {headers: this.headers});
+  }
+
+  DoctorChoosingExaminationSymptoms(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/DoctorChoosingExaminationSymptoms', dto, {headers: this.headers});
+  }
+
+  DoctorChoosingExaminationReport(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/DoctorChoosingExaminationReport', dto, {headers: this.headers});
+  }
+
+  DoctorChoosingExaminationPrescriptions(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/DoctorChoosingExaminationPrescriptions', dto, {headers: this.headers});
+  }
+
+  BackToExaminationPerscriptiosChoosing(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/BackToExaminationPerscriptiosChoosing', dto, {headers: this.headers});
+  }
+
+  BackToExaminationSymptomsChoosing(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/BackToExaminationSymptomsChoosing', dto, {headers: this.headers});
+  }
+
+  BackToExaminationReportChoosing(dto: DoctorExaminationDTO): Observable<DoctorExaminationDTO> {
+    return this.http.post<DoctorExaminationDTO>('api/DoctorExaminationEvent/BackToExaminationReportChoosing', dto, {headers: this.headers});
+  }
+}
