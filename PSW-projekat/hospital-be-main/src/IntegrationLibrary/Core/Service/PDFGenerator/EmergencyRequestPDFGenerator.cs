1c0c0d471a823d4c70f86687a518fe012044ffce
AleksandraVujovic <vujovic.ra103.2019@uns.ac.rs>
2023-01-11 20:31:43 +0100

    Create PDF Generator for emergency requests

---
+++ PSW-projekat/hospital-be-main/src/IntegrationLibrary/Core/Service/PDFGenerator/EmergencyRequestPDFGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.Core.Service.PDFGenerator
+{
+    public class EmergencyRequestPDFGenerator : IPDFService
+    {
+        public string createFileName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePDF()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePDFBody()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePDFStyle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PSW-projekat/hospital-be-main/src/IntegrationLibrary/Core/Service/PDFGenerator/IPDFService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.Core.Service.PDFGenerator
+{
+    public interface IPDFService
+    {
+        void CreatePDF();
+        String createFileName(String name);
+        void CreatePDFStyle();
+
+        void CreatePDFBody();
+    }
+}
