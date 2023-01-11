0ea11beb2d27c614501e0ee4dc9a745a21b35ddf
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2023-01-11 13:40:15 +0100

    Display stats

---
+++ PSW-projekat/PZL-Angular/src/app/modules/emergency-blood-requests/model/report.model.ts
@@ -0,0 +1,13 @@
+export class Report {
+  //   bloodQuantity: number = 0;
+  //   bloodBankID: number = -1;
+  bloodAmmounts: any = null;
+  abnBanks: any = null;
+  abpBanks: any = null;
+  anBanks: any = null;
+  apBanks: any = null;
+  bnBanks: any = null;
+  bpBanks: any = null;
+  onBanks: any = null;
+  opBanks: any = null;
+}
--- PSW-projekat/PZL-Angular/src/app/modules/emergency-blood-requests/requests-report/requests-report.component.ts
+++ PSW-projekat/PZL-Angular/src/app/modules/emergency-blood-requests/requests-report/requests-report.component.ts
@@ -1,4 +1,7 @@
+import { EmergencyBloodRequestService } from '../services/emergency-blood-request.service';
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
+import { Report } from '../model/report.model';
 
 @Component({
   selector: 'app-requests-report',
@@ -7,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestsReportComponent implements OnInit {
   public dates = { start: new Date(), end: new Date() };
-  public requests = 'asd';
-  constructor() {}
+  public errorMessage: any;
+  public report: Report = new Report();
+  constructor(
+    private emergencyService: EmergencyBloodRequestService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
   public accept() {
     console.log('accept', this.dates);
+    this.emergencyService.getReport(this.dates).subscribe(
+      (res) => {
+        console.log(res);
+        this.report = res;
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.toastr.error(error);
+      }
+    );
   }
 }
