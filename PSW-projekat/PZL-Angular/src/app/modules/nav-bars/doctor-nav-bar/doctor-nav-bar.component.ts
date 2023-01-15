5592ffbee0b7df7f5319709af8dc271ae687b1e3
galicc <ivan500galich@gmail.com>
2023-01-15 13:51:28 +0100

    DoctorNavBar ManagerNavBar

--- PSW-projekat/PZL-Angular/src/app/modules/nav-bars/doctor-nav-bar/doctor-nav-bar.component.ts
+++ PSW-projekat/PZL-Angular/src/app/modules/nav-bars/doctor-nav-bar/doctor-nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../hospital/services/login.service';
 
 @Component({
   selector: 'app-doctor-nav-bar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorNavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
+  logout(){
+    this.loginService.logout().subscribe(res => {
+      
+    })
+  }
 }
